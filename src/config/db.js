184060192f333f3eb39a486270b8c9a11d0fcc86
Priya Dharshini <priyadharshinih2@gmail.com>
2026-06-30 21:28:45 +0530

    Completed day 2 of the coding

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 const mysql2 = require('mysql2');
-require('dotenv').config();
+require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') });
 
 const pool = mysql2.createPool({
   host: process.env.DB_HOST,
